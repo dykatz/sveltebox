bc175c73f0f099e06bef1428bb917d9aa5dd2dec
Dylan Katz <dykatz@uw.edu>
2021-12-19 21:01:47 -0800

    Adaptive nav bar

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -4,6 +4,9 @@
   /** @type {boolean} */
   let showBar = false;
 
+  /** @type {boolean} */
+  let signedIn = false;
+
   /** @type {string} */
   let searchQuery = "";
 
@@ -16,9 +19,7 @@
 
 <div class="w3-bar w3-black">
   <a href="/" class="w3-bar-item w3-button">Home</a>
-  <a href="/notifications" class="w3-bar-item w3-button w3-hide-small">
-    Notifications
-  </a>
+  <a href="/about" class="w3-bar-item w3-button w3-hide-small">About</a>
 
   <form on:submit={onSearch}>
     <input
@@ -28,14 +29,35 @@
       bind:value={searchQuery}
     />
   </form>
-  <div class="w3-bar-item w3-button w3-hide-small w3-right">Post</div>
 
-  <div class="w3-dropdown-hover w3-hide-small w3-right">
-    <button class="w3-button">Account</button>
-    <div class="w3-dropdown-content w3-bar-block w3-card-4">
-      <div class="w3-bar-item w3-button">Log In</div>
+  {#if signedIn}
+    <div class="w3-dropdown-hover w3-hide-small w3-right">
+      <button class="w3-button">Profile</button>
+      <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="/notifications" class="w3-bar-item w3-button">Notifications</a>
+        <a href="/follow-requests" class="w3-bar-item w3-button">
+          Follow Requests
+        </a>
+        <a href="/chats" class="w3-bar-item w3-button">Chats</a>
+        <a href="/@me" class="w3-bar-item w3-button">My Account</a>
+        <a href="/settings" class="w3-bar-item w3-button">Settings</a>
+        <button
+          class="w3-bar-item w3-button"
+          on:click={() => (signedIn = false)}
+        >
+          Sign Out
+        </button>
+      </div>
     </div>
-  </div>
+    <div class="w3-bar-item w3-button w3-hide-small w3-right">Post</div>
+  {:else}
+    <button
+      class="w3-bar-item w3-button w3-hide-small w3-right"
+      on:click={() => (signedIn = true)}
+    >
+      Sign In
+    </button>
+  {/if}
 
   <button
     class="w3-button w3-hide-medium w3-hide-large w3-right"
@@ -43,27 +65,35 @@
   >
 </div>
 
-<div
-  class="w3-bar-block w3-black w3-hide-medium w3-hide-large"
-  class:w3-hide={!showBar}
->
-  <a href="/notifications" class="w3-bar-item w3-button">Notifications</a>
+{#if showBar}
+  <div class="w3-bar-block w3-black w3-hide-medium w3-hide-large">
+    <a href="/about" class="w3-bar-item w3-button">About</a>
 
-  <div class="w3-bar-item w3-button">Post</div>
+    {#if signedIn}
+      <div class="w3-bar-item w3-button">Post</div>
+      <a href="/notifications" class="w3-bar-item w3-button">Notifications</a>
+      <a href="/follow-requests" class="w3-bar-item w3-button">
+        Follow Requests
+      </a>
+      <a href="/chats" class="w3-bar-item w3-button">Chats</a>
+      <a href="/@me" class="w3-bar-item w3-button">My Account</a>
+      <a href="/settings" class="w3-bar-item w3-button">Settings</a>
+      <div class="w3-bar-item w3-button" on:click={() => (signedIn = false)}>
+        Sign Out
+      </div>
+    {:else}
+      <button class="w3-bar-item w3-button" on:click={() => (signedIn = true)}>
+        Sign In
+      </button>
+    {/if}
 
-  <div class="w3-dropdown-hover">
-    <button class="w3-button">Account</button>
-    <div class="w3-dropdown-content w3-bar-block w3-card-4">
-      <div class="w3-bar-item w3-button">Log In</div>
-    </div>
+    <form on:submit={onSearch}>
+      <input
+        class="w3-input w3-bar-item w3-border"
+        type="text"
+        placeholder="Search"
+        bind:value={searchQuery}
+      />
+    </form>
   </div>
-
-  <form on:submit={onSearch}>
-    <input
-      class="w3-input w3-bar-item w3-border"
-      type="text"
-      placeholder="Search"
-      bind:value={searchQuery}
-    />
-  </form>
-</div>
+{/if}
